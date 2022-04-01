3276789e9c6a2f79aa86d4cc07d62fc9c3418ab6
xingyebubai <1253076882@qq.com>
2022-04-01 23:26:37 +0800

    feat: 点击图片后跳转到详情页

---
+++ src/view/recommend/MovieDetails.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { NGradientText } from "naive-ui";
+
+interface Props {
+  id: string;
+}
+defineProps<Props>();
+
+</script>
+<template>
+  <n-gradient-text>电影详情页: {{ id }}</n-gradient-text>
+</template>
+<style lang="scss" scoped></style>
